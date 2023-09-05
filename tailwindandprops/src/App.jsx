b8148d61862a3a4010b8c43bf57dc90354d0020f
Nabin Garai <nabingarai259@gmail.com>
2023-09-05 21:55:21 +0530

    backgroundChanger update

---
+++ tailwindandprops/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username="Nabin"/>
+      <Card username="Garai"/>
+    </>
+  )
+}
+
+export default App
